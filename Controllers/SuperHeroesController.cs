bfc69dab5a10a82772c690dd5b12a7fed333d23c
yaadin~ <depsp6600@Meder.local>
2023-08-12 16:44:22 +0800

    added async functions to get elements

--- Controllers/SuperHeroesController.cs
+++ Controllers/SuperHeroesController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Data;
 using SuperHeroAPI.Models;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -7,11 +9,50 @@ namespace SuperHeroAPI.Controllers
     [Route("api/heroes")]
     public class SuperHeroesController : Controller
     {
+        private readonly HeroesDbContext _db;
+        public SuperHeroesController(HeroesDbContext db)
+        {
+            _db = db;
+        }
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetAllHeroes()
         {
-            return Ok();
+            try
+            {
+                var elements = await _db.hero.ToListAsync();
+                if (elements == null)
+                {
+                    return NotFound();
+                }
+                return Ok(elements);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occured: {ex.Message}");
+            }
+        }
+        [HttpGet("id")]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<List<HeroModel>>> GetbyId(int id)
+        {
+            try
+            {
+                var element = await _db.hero.FirstOrDefaultAsync(u => u.ID == id);
+                if (element == null)
+                {
+                    return NotFound();
+                }
+                return Ok(element);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occured: {ex.Message}");
+            }
+          
         }
     }
-}
 
+}
